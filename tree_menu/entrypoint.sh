b18f89f216e539aa21ef3cb3518c91adf9cb7d05
Treglazov Vladislav <threeeyesvl@gmail.com>
2024-08-14 20:36:59 +0300

    Add docker

---
+++ tree_menu/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$DEBUG" = "True" ]; then
+    python manage.py migrate
+    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    python manage.py collectstatic --noinput --clear
+fi
+
+gunicorn tree_menu.wsgi:application --bind 0.0.0.0:8000
